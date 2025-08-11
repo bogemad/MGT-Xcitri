3dd2c1a9db95c8b83de5108989b5d81df32dda7b
Daniel Bogema <daniel.bogema@dpi.nsw.gov.au>
2025-08-11 12:09:20 +1000

    cron_pipeline fix

--- Mgt/Mgt/Mgt/settings_template.py
+++ Mgt/Mgt/Mgt/settings_template.py
@@ -179,9 +179,9 @@ SERVER_EMAIL = os.getenv("SERVER_EMAIL", DEFAULT_FROM_EMAIL)
 #RELATIVE PATHS FROM folder containing manage.py in this repo to folder on your system
 # NOTE: Please move the data folders to a secure location once setup is complete.  
 SUBDIR_REFERENCES = '../../References/' 
-SUBDIR_ALLELES = '../../MGT-Xcitri/Alleles/' 
-MEDIA_ROOT = '../../MGT-Xcitri/Uploads/'
-BLASTALLELES='../../MGT-Xcitri/species_specific_alleles/'
+SUBDIR_ALLELES = '../../Alleles/' 
+MEDIA_ROOT = '../../Uploads/'
+BLASTALLELES='../../species_specific_alleles/'
 
 # ABSOLUTE PATH VERSIONS OF ABOVE
 ABS_SUBDIR_REFERENCES = os.path.join(BASE_DIR, 'References/')
